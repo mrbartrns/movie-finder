cf834fa27800794b11b9267a059d24d468f5cbf7
Junhyuck Ko <mrbartrns@naver.com>
2022-05-17 17:21:00 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,12 @@ module.exports = {
     },
   },
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
-    clean: true,
-  },
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   publicPath: '/',
+  //   filename: 'bundle.js',
+  //   clean: true,
+  // },
   module: {
     rules: [
       {
