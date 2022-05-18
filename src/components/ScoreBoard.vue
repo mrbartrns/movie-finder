a8f7c2e4106d1813cb0963a834c0ac24f80a078e
Junhyuck Ko <mrbartrns@naver.com>
2022-05-18 19:02:08 +0900

    feat: add score board

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="score-board">
+    <div class="score-board__score"><slot name="score"></slot></div>
+    <div class="score-board__company"><slot name="company"></slot></div>
+  </div>
+</template>
+<style
+  lang="scss"
+  scoped
+>
+@use '@/scss/_vars.scss' as vars;
+@use '@/scss/_mixins.scss' as mixins;
+
+.score-board {
+  flex-basis: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 120px;
+  padding: 1rem;
+  border: 1px solid vars.$gray;
+  border-radius: 0.5rem;
+
+  &__score {
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+
+  &__company {
+    font-weight: 500;
+    font-size: 0.825rem;
+  }
+
+  @include mixins.mobile {
+    flex: 1;
+    width: 100%;
+  }
+}
+
+.imdb {
+  background-color: #ffd24c;
+}
+
+.rotten-tomatoes {
+  color: white;
+  background-color: #fa320a;
+}
+
+.metacritic {
+  color: white;
+  background-color: #111111;
+}
+</style>
