6e8f96497e203b10531f1c573bc416a193905522
Junhyuck Ko <mrbartrns@naver.com>
2022-05-29 22:59:32 +0900

    refactor: iterate movie items

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    v-if="movieInfo[1] && movieInfo[1] !== 'N/A'"
+    class="movie__item"
+  >
+    {{ info }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    movieInfo: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    info() {
+      return `${this.movieInfo[0]}: ${this.movieInfo[1]}`;
+    },
+  },
+};
+</script>
